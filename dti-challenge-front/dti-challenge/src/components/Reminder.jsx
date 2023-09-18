43a086a23a0cf1bd3ff89dfffb83ec2ffd9e6e38
Ernesto Queiroz <ernestoqueiroz2@gmail.com>
2023-09-18 20:31:41 -0300

    Fixing console log

--- dti-challenge-front/dti-challenge/src/components/Reminder.jsx
+++ dti-challenge-front/dti-challenge/src/components/Reminder.jsx
@@ -1,6 +1,5 @@
 function Reminder(props) {
     function deleteReminder(id) {
-      console.log("FUI CHAMADO PORRA!")
       fetch("http://localhost:5242/reminders/" + id, {
             method: 'DELETE', 
             mode: 'cors',
